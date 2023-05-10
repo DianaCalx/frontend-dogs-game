7c42f9ef7675ce55c0f778d4335da161c13fe0a3
Diana Calderon <dianacalderon@ravn.co>
2023-05-10 17:36:51 -0600

    Update useRandomOptions hook logic

--- src/hooks/useRandomOptions.tsx
+++ src/hooks/useRandomOptions.tsx
@@ -1,27 +1,37 @@
 import { useState } from 'react'
 
-type SetNewRandomListParams = {
+type SetRandomParams = {
   list: string[]
-  current: string
+  current?: string
+  number?: number
 }
 
-const useRandomOptions = (number = 4) => {
-  const [randomItems, setRandomItems] = useState<string[]>([])
+const useRandomOptions = () => {
+  const [randomOptions, setRandomOptions] = useState<string[]>([])
 
-  const setNewRandomList = ({ list, current }: SetNewRandomListParams) => {
-    const elements: string[] = [current]
+  const getNewRandomList = ({ list, current, number = 4 }: SetRandomParams) => {
+    const elements: string[] = []
 
-    for (let i = 1; i < number; i++) {
+    if (current) elements.push(current)
+  
+    for (let i = current ? 1 : 0; i < number; i++) {
       const rest = list.filter(item => !elements.some(element => element === item))
       const newElement = rest[Math.floor(Math.random() * rest.length)]
       elements.push(newElement)
     }
-    setRandomItems(elements.sort())
+    
+    return elements.sort()
+  }
+
+  const setNewRandomOptions = ({ list, current, number = 4 }: SetRandomParams) => {
+    const getRandomItems = getNewRandomList({ list, current, number })
+    setRandomOptions(getRandomItems)
   }
 
   return {
-    randomItems,
-    setNewRandomList
+    randomOptions,
+    setNewRandomOptions,
+    getNewRandomList
   }
 }
 
