5ca39598f4f4b1f52696fc9cde2034d573c46e8f
Diana Calderon <dianacalderon@ravn.co>
2023-05-09 21:08:08 -0600

    Add config and styles to BreedsLit component

--- src/components/BreedsList.tsx
+++ src/components/BreedsList.tsx
@@ -1,8 +1,29 @@
 
+import { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import { ReactComponent as CheckMark } from '../assets/check-mark.svg';
+
+import './BreedsList.css';
 
 const BreedsList = () => {
+  const { breeds, guessed, isCheatMode, switchMode } = useContext(AppContext)
+
+  const isGuessedBreed = (breed: string) => {
+    const isGuessed = guessed.some(item => item === breed)
+    return isGuessed
+  }
+  
   return (
-    <div>BreedsList</div>
+    <>
+      <button onClick={switchMode} className='breedslist__button'>Swich game to {isCheatMode ? 'normal' : 'cheat'} mode</button>
+      <h2 className='breedslist__title'>List of breeds</h2>
+      {breeds.map(breed => (
+        <div key={breed} className='breed'>
+          {isGuessedBreed(breed) && <CheckMark  className='breed__checkmark'/>}
+          <p className='breed__name'>{breed}</p>
+        </div>
+      ))}
+    </>
   )
 }
 
