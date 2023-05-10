877a7efeaa41e143bdeaedc0ebeef8528e99df04
Diana Calderon <dianacalderon@ravn.co>
2023-05-10 08:18:45 -0600

    Use Spinner in Content Component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,9 +1,19 @@
+import { useContext } from 'react'
 import BreedsList from './BreedsList'
 import Questionnaire from './Questionnaire'
+import { AppContext } from '../context/AppContext'
+import Spinner from './Spinner'
 
 import './Content.css';
 
 const Content = () => {
+
+  const { breeds } = useContext(AppContext)
+
+  if (!breeds.length) {
+    return <Spinner />
+  }
+  
   return (
     <div className='content__container'>
       <div className='content_breedslist'>
