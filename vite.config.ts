538b01709182c579b4961b365c6d5d40c1a3e656
Diana Calderon <dianacalderon@ravn.co>
2023-05-10 09:14:11 -0600

    Allow netlify using env vars

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
+const viteEnv = {}
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key]
+  }
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  define: viteEnv
 })
